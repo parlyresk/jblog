b576ae5d8d18e5334766c9d17441012628da5456
parlyresk <parlyresk325@gmail.com>
2024-06-14 17:37:36 +0900

    auth 권한 설정

--- jblog3/src/main/java/com/poscodx/jblog/controller/UserController.java
+++ jblog3/src/main/java/com/poscodx/jblog/controller/UserController.java
@@ -13,46 +13,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.poscodx.jblog.service.UserService;
 import com.poscodx.jblog.vo.UserVo;
 
-
-
-
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserService userService;
-	
-	@RequestMapping(value="/join",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String join(@ModelAttribute UserVo vo) {
-		System.out.println("user join get");
+
 		return "user/join";
-		
+
 	}
-	
-	@RequestMapping(value="/join",method=RequestMethod.POST)
+
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
 	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
-		System.out.println("user join post");
-		if(result.hasErrors()) {
-			
+
+		if (result.hasErrors()) {
 
 			model.addAllAttributes(result.getModel());
 			return "user/join";
 		}
-		
+
 		userService.join(vo);
-		
-		
+
 		return "redirect:/user/joinsuccess";
-		
+
 	}
-	
-	@RequestMapping(value="/joinsuccess",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/joinsuccess", method = RequestMethod.GET)
 	public String joinsuccess() {
 		return "user/joinsuccess";
-		
+
 	}
-	
-	@RequestMapping(value="/login",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "user/login";
 	}
--- jblog3/src/main/java/com/poscodx/jblog/security/AuthInterceptor.java
+++ jblog3/src/main/java/com/poscodx/jblog/security/AuthInterceptor.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.poscodx.jblog.vo.UserVo;
+
 public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
@@ -28,6 +30,15 @@ public class AuthInterceptor implements HandlerInterceptor {
 
         HttpSession session = request.getSession();
         if (session == null || session.getAttribute("authUser") == null) {
+            response.sendRedirect(request.getContextPath() + "/user/login");
+            return false;
+        }
+        
+     
+        UserVo authUser = (UserVo) session.getAttribute("authUser");
+        String urlId = request.getRequestURI().split("/")[2]; 
+
+        if (!authUser.getId().equals(urlId)) {
             response.sendRedirect(request.getContextPath() + "/user/login");
             return false;
         }
